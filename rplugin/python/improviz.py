f5d5dc7ae0ad77161019c5b3c952f1016c0023f6
Guy John <guy@rumblesan.com>
2017-05-17 19:34:23 +0100

    Add function for toggling text display

--- rplugin/python/improviz.py
+++ rplugin/python/improviz.py
@@ -22,3 +22,18 @@ class Main(object):
             self.vim.err_write("URL Error: %s\n" % str(e.reason))
         except urllib2.HTTPError, e:
             self.vim.err_write("HTTP Error: %s\n" % str(e.code))
+
+    @neovim.function('ImprovizToggleText')
+    def improvizToggleTxt(self, args):
+        self.vim.out_write("Sending to Improviz\n")
+        try:
+            url = "http://%s:%s/toggle/text" % (
+                self.vim.vars['improviz_host'],
+                self.vim.vars['improviz_port']
+            )
+            resp = urllib2.urlopen(url, data="")
+            self.vim.out_write("%s\n" % resp.read())
+        except urllib2.URLError, e:
+            self.vim.err_write("URL Error: %s - %s\n" % (str(e.reason), url))
+        except urllib2.HTTPError, e:
+            self.vim.err_write("HTTP Error: %s\n" % str(e.code))
