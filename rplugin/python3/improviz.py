3fa40f28d0834ac4dce28f03e196cdfe7bdde6b1
Guy John <guy@rumblesan.com>
2017-12-18 20:54:04 +0000

    Add function for time nudging

--- rplugin/python3/improviz.py
+++ rplugin/python3/improviz.py
@@ -25,12 +25,32 @@ class Main(object):
             self.vim.err_write("HTTP Error: %s\n" % str(e.code))
 
     @neovim.function('ImprovizToggleText')
-    def improvizToggleTxt(self, args):
+    def improvizToggleText(self, args):
         self.vim.out_write("Sending to Improviz\n")
         try:
             url = "http://%s:%s/toggle/text" % (
                 self.vim.vars['improviz_host'],
                 self.vim.vars['improviz_port']
+            )
+            resp = urlopen(url, data=str.encode(""))
+            self.vim.out_write("%s\n" % resp.read())
+        except URLError as e:
+            self.vim.err_write("URL Error: %s - %s\n" % (str(e.reason), url))
+        except HTTPError as e:
+            self.vim.err_write("HTTP Error: %s\n" % str(e.code))
+
+    @neovim.function('ImprovizNudgeBeat')
+    def improvizNudgeBeat(self, args):
+        if args[0]:
+            t = str(args[0])
+        else:
+            t = "0.25"
+        self.vim.out_write("Nudging Beat %s\n".format(t))
+        try:
+            url = "http://%s:%s/nudge/%s" % (
+                self.vim.vars['improviz_host'],
+                self.vim.vars['improviz_port'],
+                t
             )
             resp = urlopen(url, data=str.encode(""))
             self.vim.out_write("%s\n" % resp.read())
